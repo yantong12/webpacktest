3688c67688f5d627d2263c03e1c32192ad446dd8
qiyu <1582562665@qq.com>
2018-09-18 17:40:40 +0800

    起步

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './userImg.jpeg'
-import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-    var myIcon = new Image()
-    myIcon.src = Icon
-    
-    element.appendChild(myIcon)
-    console.log(Data)
 
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,4 @@ module.exports = {
     filename: "bundel.js",
     path: path.resolve(__dirname, "dist")
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: [
-            'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-            'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-            'xml-loader'
-        ]
-      }
-    ]
-  }
 };
