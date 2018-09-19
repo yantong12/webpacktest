ede300c0156e529de3630ba5f65649d9d4ed2059
qiyu <1582562665@qq.com>
2018-09-19 14:06:23 +0800

    管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app:"./src/index.js",
+    print: "./src/print.js"},
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: "bundel.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
 };
