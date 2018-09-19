eaa7e5c94e8633c3a755fbc2d1dbfc29a380c419
qiyu <1582562665@qq.com>
2018-09-19 14:34:32 +0800

    开发

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   entry: {
     app:"./src/index.js",
-    print: "./src/print.js"},
+    print: "./src/print.js"
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
@@ -14,6 +19,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/'
   },
 };
