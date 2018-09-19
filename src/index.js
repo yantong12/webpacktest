49f723bb3c9d74d1d7ab1a0021e4dd963a113876
qiyu <1582562665@qq.com>
2018-09-19 15:23:46 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import printMe from './print'
+import './style.css'
 
 function component() {
     var element = document.createElement('div');
@@ -12,8 +13,15 @@ function component() {
     btn.onclick = printMe
 
     element.appendChild(btn)
-
+    console.log('111eee')
     return element;
   }
   
   document.body.appendChild(component());
+
+  if (module.hot) {
+    module.hot.accept('./print.js',function () {
+      console.log('Accepting the updated printNe module!')
+      printMe()
+    })
+  }
--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe () {
-    console.log('i get called from print.js !')
+    // console.log('i get called from print.js !')
+    console.log('updating print.js.000')
 }
